695aa1a98294e06ed2d72c47409161dad5b95ca9
SandorMore <msaso0725@gmail.com>
2025-07-26 19:09:12 +0200

    asd

--- spmeshi.py
+++ spmeshi.py
@@ -6,5 +6,13 @@ for i in "Sigma":
     print(i, end="\t")
 
 print(*range(1,100), sep=" ", end="\t")
-std::cout << "Sigma" << std::endl;
+
 print('\"negate\"')
+
+class Sigma:
+    def __init__(self, kor : int):
+        print(kor)
+
+Sigma1 = Sigma(12)
+
+print(Sigma1)
